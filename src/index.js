6a63a5ea66ab7c8c5b44adeba120fcaa4fd4a8e2
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2018-02-02 15:50:20 +0800

    postMessage

--- src/index.js
+++ src/index.js
@@ -1,166 +1,117 @@
 import {
   WebView,
-  /* View, */
   Platform,
 } from 'react-native';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const toString = (obj) => {
-  let result = JSON.stringify(obj, (key, val) => {
-    if (typeof val === 'function') {
-      return `~--demo--~${val}~--demo--~`;
-    }
-    return val;
-  });
-
-  do {
-    result = result.replace('"~--demo--~', '');
-    result = result.replace('~--demo--~"', '');
-    result = result.replace(/\\n/g, '');
-    result = result.replace(/\\"/g, '"');
-  } while (result.indexOf('~--demo--~') >= 0);
-  return result;
-};
-
-const unicodeTransformChinese = (str) => {
-  let ret = str;
-  ret = ret.replace(
-    /\\u([0-9a-fA-F]{2,4})/g,
-    (string, matched) => {
-      let chinese = parseInt(matched, 16);
-      chinese = String.fromCharCode(chinese);
-      return chinese;
-    },
-  );
-  return ret;
-};
+const echartHtml = require('./echart.html');
 
 export default class Echarts extends Component {
   constructor() {
     super();
-    this.state = {
-      width: 0,
-      height: 0,
-      option: '',
-    };
+    this.load = false;
   }
   componentWillMount() {
-    const {
-      width,
-      height,
-      option: _option,
-    } = this.props;
-    this.state.width = width;
-    this.state.height = height;
-    this.state.option = this.getOption(_option);
   }
   componentDidMount() {
     this._isMounted = true;
   }
-  componentWillReceiveProps({ option: _option }) {
-    if (_option !== this.props.option) {
-      this.state.option = this.getOption(_option);
-      const js = `
-        myChart.setOption(${this.state.option});
-      `;
-      this.chart.injectJavaScript(js);
-    }
-  }
-  shouldComponentUpdate(props) {
-    const thisProps = this.props || {};
-    const nextProps = props || {};
-    if (Object.keys(thisProps).length !== Object.keys(nextProps).length) {
-      return true;
-    }
-
-    for (const key in nextProps) {
-      if (JSON.stringify(thisProps[key]) !== JSON.stringify(nextProps[key])) {
-        return true;
+  async componentWillReceiveProps({ option }) {
+    if (option !== this.props.option) {
+      const message = this.stringify(option);
+      for (;;) {
+        await new Promise((r) => setTimeout(r, 100));
+        if (this.load) {
+          this.chart.postMessage(message);
+          return;
+        }
       }
     }
+  }
+  shouldComponentUpdate() {
     return false;
   }
   componentWillUnmount() {
     this._isMounted = false;
   }
-  getOption(option) {
-    let ret = toString(option);
-    ret = unicodeTransformChinese(ret);
-    return ret;
-  }
   getRenderChartJsCode() {
     const {
       height,
       width,
-      option,
-    } = this.state;
+    } = this.props;
     return `
       document.getElementById('main').style.height = "${height}px";
       document.getElementById('main').style.width = "${width}px";
       var myChart = echarts.init(document.getElementById('main'));
-      myChart.setOption(${option});
-      myChart.on('click', function(params) {
-        var seen = [];
-        var paramsString = JSON.stringify(params, function(key, val) {
-          if (val != null && typeof val == "object") {
-            if (seen.indexOf(val) >= 0) {
-              return;
-            }
-            seen.push(val);
+      myChart.setOption({});
+      myChart.on('click', function (){
+        window.postMessage('aa');
+      });
+      var parse = function (str) {
+        const result = JSON.parse(str, function(key, val) {
+          if (
+            typeof val === 'string'
+            && val.match(/~~function~~/g)
+          ) {
+            var code = val.replace(/~~function~~/g, '');
+            code = eval('(' + code + ')');
+            return code;
           }
           return val;
         });
-        window.postMessage(paramsString);
-      });
+        return result;
+      };
+      var handleMessage = function (_message) {
+        try {
+          var message = parse(_message.data);
+          myChart.setOption(message);
+        } catch (e) {
+          window.postMessage(e);
+        }
+      };
+      window.document.addEventListener('message', handleMessage);
     `;
   }
-  _setState(state) {
-    if (this._isMounted) {
-      this.setState(state);
+  getEchartHtml() {
+    if (Platform.OS === 'ios' || __DEV__) {
+      return echartHtml;
     }
+
+    return {
+      uri: 'file:///android_asset/echarts.html',
+    };
+  }
+  stringify(obj) {
+    const result = JSON.stringify(obj, (key, val) => {
+      if (typeof val === 'function') {
+        return `~~function~~${val}~~function~~`;
+      }
+      return val;
+    });
+    /* result = unicodeTransformChinese(result); */
+    return result;
   }
   handleRefs = (chart) => {
     this.chart = chart;
   }
-  handleOnMessage = ({ nativeEvent }) => {
-    const {
-      onPress,
-    } = this.props;
-    if (onPress) {
-      const data = JSON.parse(nativeEvent.data);
-      onPress(data);
-    }
+  handleMessage = ({ nativeEvent }) => {
+  }
+  handleWebViewLoadend = () => {
+    this.load = true;
   }
   render() {
-    /*
-     * console.log(
-     *   this.state,
-     *   'this.state',
-     * );
-     */
     const {
       height,
       width,
     } = this.props;
-    /* const source = (Platform.OS === 'ios' || __DEV__) */
-    const source = Platform.OS === 'ios'
-      ? require('./tpl.html')
-      : {
-        uri: 'file:///android_asset/echarts.html',
-      };
-    const js = this.getRenderChartJsCode();
-    /*
-     * console.log(
-     *   js,
-     *   'js',
-     * );
-     */
     const webViewStyle = {
       height,
       width,
       backgroundColor: 'transparent',
     };
+    const js = this.getRenderChartJsCode();
+    const source = this.getEchartHtml();
     return (
       <WebView
         style={webViewStyle}
@@ -169,7 +120,8 @@ export default class Echarts extends Component {
         injectedJavaScript={js}
         scalesPageToFit={Platform.OS === 'android'}
         source={source}
-        onMessage={this.handleOnMessage}
+        onMessage={this.handleMessage}
+        onLoadEnd={this.handleWebViewLoadend}
       />
     );
   }
@@ -186,3 +138,4 @@ Echarts.propTypes = {
   height: PropTypes.number,
   option: PropTypes.object,
 };
+
